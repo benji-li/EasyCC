1e7f624fe1c8ea13eec96cd0fc4604cc411db4a8
benjelloo <55926840+benjelloo@users.noreply.github.com>
2021-01-10 10:18:38 -0800

    changed window type to popup

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').addEventListener('click',onclick,false)
     function onclick(){
-        chrome.windows.create({height:30,url:'./dialog/caption.html',top:600});
+        chrome.windows.create({height:300,url:'./dialog/caption.html',top:600,type:'popup'});
         chrome.tabs.query({currentWindow: true,active:true},
         function(tabs){
             chrome.tabCapture.capture({audio:true, video:false},(stream)=>{
